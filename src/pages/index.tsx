f55bef9b2af8e4b570c4fa4353fcfaf16c0be3c8
Jan Moták <61924776+motakjan@users.noreply.github.com>
2023-04-12 22:10:52 +0200

    Clerk setup and user in context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import styles from "./index.module.css";
-import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
+import { SignInButton, UserButton } from '@clerk/nextjs';
+import { type NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from './index.module.css';
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api';
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.example.hello.useQuery({ text: 'from tRPC' });
 
   return (
     <>
@@ -45,8 +46,10 @@ const Home: NextPage = () => {
             </Link>
           </div>
           <p className={styles.showcaseText}>
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            {hello.data ? hello.data.greeting : 'Loading tRPC query...'}
           </p>
+          <SignInButton />
+          <UserButton />
         </div>
       </main>
     </>
