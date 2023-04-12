7277771db0d9b69ef82626b6010a566c5e8bdccd
Jan Moták <61924776+motakjan@users.noreply.github.com>
2023-04-12 22:21:37 +0200

    Basic mantine setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,22 @@ import { type AppType } from 'next/app';
 import { api } from '~/utils/api';
 
 import { ClerkProvider } from '@clerk/nextjs';
+import { MantineProvider } from '@mantine/core';
 import '~/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <MantineProvider
+      theme={{
+        colorScheme: 'dark',
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </MantineProvider>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { SignInButton, UserButton } from '@clerk/nextjs';
 import { type NextPage } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import styles from './index.module.css';
 
+import { Switch } from '@mantine/core';
 import { api } from '~/utils/api';
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: 'from tRPC' });
+
+  console.log(hello);
 
   return (
     <>
@@ -18,36 +20,7 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <div className={styles.container}>
-          <h1 className={styles.title}>
-            Create <span className={styles.pinkSpan}>T3</span> App
-          </h1>
-          <div className={styles.cardRow}>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>First Steps →</h3>
-              <div className={styles.cardText}>
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className={styles.card}
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className={styles.cardTitle}>Documentation →</h3>
-              <div className={styles.cardText}>
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
-          <p className={styles.showcaseText}>
-            {hello.data ? hello.data.greeting : 'Loading tRPC query...'}
-          </p>
+          <Switch label="I agree to sell my privacy" />
           <SignInButton />
           <UserButton />
         </div>
