fc399d043a0f6fad7e235b830ab93cf492737e68
Jan Moták <61924776+motakjan@users.noreply.github.com>
2023-04-13 20:50:31 +0200

    Basic layout setup

---
+++ src/pages/components/Layout/Navbar/NavbarLink.tsx
@@ -0,0 +1,33 @@
+import { Tooltip, UnstyledButton } from '@mantine/core';
+import Link from 'next/link';
+import type { NavbarLinkProps } from '../Layout.types';
+import { useStyles } from './Navbar.styles';
+
+export const NavbarLink: React.FC<NavbarLinkProps> = ({
+  icon: Icon,
+  label,
+  active,
+  href,
+  onClick,
+}) => {
+  const { classes, cx } = useStyles();
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      {onClick ? (
+        <UnstyledButton
+          onClick={onClick}
+          className={cx(classes.link, { [classes.active]: active })}
+        >
+          <Icon size="1.2rem" stroke={1.5} />
+        </UnstyledButton>
+      ) : (
+        <Link
+          href={href as string}
+          className={cx(classes.link, { [classes.active]: active })}
+        >
+          <Icon size="1.2rem" stroke={1.5} />
+        </Link>
+      )}
+    </Tooltip>
+  );
+};
